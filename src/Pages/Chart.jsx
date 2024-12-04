62490000997a960e08505babf26aef4a86912b17
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-04 15:45:52 +0600

    .

--- src/Pages/Chart.jsx
+++ src/Pages/Chart.jsx
@@ -8,7 +8,7 @@ const Chart = () => {
       <div className='bg-white shadow rounded-xl'>3</div>
       <div className='bg-white shadow rounded-xl'>4</div>
       <div className='bg-white shadow rounded-xl'>5</div>
-      <div className='bg-white shadow rounded-xl row-span-2 col-span-1'>6</div>
+      <div className='bg-gradient-to-b from-[#6434F8] to-[#312787] shadow rounded-xl row-span-2 col-span-1'></div>
       <div className='bg-white shadow rounded-xl row-span-2 col-span-1 mt-[30px]'>7</div>
       <div className='bg-white shadow rounded-xl row-span-2 col-span-3'>8</div>
     </div>
